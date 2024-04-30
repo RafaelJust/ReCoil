fe93c26d61be90d6b41e52721d52f3166328b1c6
Rafael Just <justrafito@gmail.com>
2024-04-30 14:10:40 +0200

    Added bullet despawn. Bullet looks at velocity now. Tweaked some player physics.

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -5,3 +5,13 @@ var initialForce: float
 func shoot() -> void:
 	print(initialForce)
 	apply_central_force(Vector2.from_angle(rotation) * initialForce)
+	%Life.wait_time = randf_range(1,3)
+	%Life.start()
+
+func _physics_process(_delta: float) -> void:
+	var dir: Vector2 = linear_velocity
+	%Body.look_at(position + dir)
+
+
+func _on_life_timeout() -> void:
+	queue_free()
