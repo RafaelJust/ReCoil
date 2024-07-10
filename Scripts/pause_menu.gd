7c124bcf9993fcbac58b87fac6e8cb68199d3f70
Rafael Just <RafaelJust@proton.me>
2024-07-10 22:03:51 +0200

    Make the sliders actually do something, and add pause audio

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -1,5 +1,16 @@
 extends Control
 @export var GameController: Node2D
+@export var volumeCurve: Curve
+
+var musicFadeInTimer: float
+
+func _process(delta):
+	if self.visible && musicFadeInTimer < 10:
+		musicFadeInTimer += delta
+		$Music.volume_db = volumeCurve.sample(musicFadeInTimer / 10)
+	elif musicFadeInTimer > 0:
+		musicFadeInTimer = 0
+		$Music.volume_db = -72
 
 func _on_continue_button_pressed():
 	GameController.continue_game()
@@ -7,3 +18,15 @@ func _on_continue_button_pressed():
 
 func _on_exit_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Title.tscn")
+
+
+func _on_master_volume_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(0, volumeCurve.sample(value / 100))
+
+
+func _on_music_volume_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(1, volumeCurve.sample(value / 100))
+
+
+func _on_sfx_volume_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(2, volumeCurve.sample(value / 100))
