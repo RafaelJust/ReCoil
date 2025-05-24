581b8bff11e76b1b78d185e548a612e9334b241c
Rafael Just <rafaeljust@proton.me>
2025-05-24 18:24:26 +0200

    Add highscore to title screen

--- Scripts/Main_Menu/Manager.gd
+++ Scripts/Main_Menu/Manager.gd
@@ -1,14 +1,21 @@
-extends Control
-var mainScene: PackedScene = preload("res://Scenes/main.tscn")
+extends Control;
+var mainScene: PackedScene = preload("res://Scenes/main.tscn");
+
+func _ready() -> void:
+	var highscore = 0;
+	var file = FileAccess.open("user://scores.dat", FileAccess.READ);
+	if file:
+		highscore = file.get_64()
+	$HighScore.text = "Highscore [color=yellow][b] %d" % highscore
 
 func Play() -> void:
-	get_tree().paused = false
-	$AudioStreamPlayer.play()
-	$AnimationPlayer.play("Fade_out")
+	get_tree().paused = false;
+	$AudioStreamPlayer.play();
+	$AnimationPlayer.play("Fade_out");
 
 func Quit() -> void:
-	get_tree().quit()
+	get_tree().quit();
 
 
 func startGame(_anim_name):
-	get_tree().change_scene_to_packed(mainScene)
+	get_tree().change_scene_to_packed(mainScene);
