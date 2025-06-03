cb7067857e2f57b83eed8a873a6e0ee5a2b89f7c
Rafael Just <rafaeljust@proton.me>
2025-06-03 12:36:28 +0200

    Add flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
   description = "A very basic flake";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+  inputs = {
+      nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
 
   outputs = { self, nixpkgs }: {
-    pkgs = import nixpkgs { };
-    packages.x86_64-linux.default =
-      self.packages.x86_64-linux.callPackage ./nix { };
+    packages.x86_64-linux.default = nixpkgs.legacyPackages."x86_64-linux".callPackage ./nix { };
   };
 }
